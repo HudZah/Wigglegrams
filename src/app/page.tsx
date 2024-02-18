4d173f295c56001bab23d867068b00ecc431d61a
Hudzah <chiefhudhayfa@gmail.com>
2024-02-18 17:07:04 -0500

    Commit diff

---
+++ src/app/page.tsx
@@ -0,0 +1,50 @@
+import * as React from "react";
+
+import { Card, CardContent } from "@/components/ui/card";
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from "@/components/ui/carousel";
+
+export default function Home() {
+    return (
+        <>
+            <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+                <div className="flex items-center justify-between space-y-2">
+                    <div>
+                        <h2 className="text-2xl font-bold tracking-tight">
+                            Wigglegrams
+                        </h2>
+                        <p className="text-muted-foreground">
+                            Creaet wigglegrams in just a few clicks
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="flex justify-center items-center h-[70%]">
+                <Carousel className="w-full max-w-xl">
+                    <CarouselContent>
+                        {Array.from({ length: 5 }).map((_, index) => (
+                            <CarouselItem key={index}>
+                                <div className="p-1">
+                                    <Card>
+                                        <CardContent className="flex aspect-square items-center justify-center p-6">
+                                            <span className="text-4xl font-semibold">
+                                                {index + 1}
+                                            </span>
+                                        </CardContent>
+                                    </Card>
+                                </div>
+                            </CarouselItem>
+                        ))}
+                    </CarouselContent>
+                    <CarouselPrevious />
+                    <CarouselNext />
+                </Carousel>
+            </div>
+        </>
+    );
+}
